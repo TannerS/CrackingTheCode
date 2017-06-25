b599c7a14224d4de476bd9aa0b8b343955a86689
Tanner Summers <tannersummers@gmail.com>
2017-06-24 21:55:17 -0700

    Initial commit

---
+++ src/io/tanners/PermutationStrings.java
@@ -0,0 +1,91 @@
+package io.tanners;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * See if a string is a permutation of the other
+ * 1.2
+ *
+ *
+ */
+public class PermutationStrings
+{
+    public static void main(String[] args) {
+        String testCase1 = "tac";
+        String testCase2 = "cat";
+
+        String testCase3 = "dog";
+        String testCase4 = "god";
+
+        String testCase5 = "tic";
+        String testCase6 = "tac";
+
+        String testCase7 = "dogg";
+        String testCase8 = "god";
+
+
+
+
+        System.out.println("TAC?CAT: " + func1("tac", "cat"));
+        System.out.println("DOG?GOD: " + func1("dog", "god"));
+        System.out.println("TIC?TAC: " + func1("tic", "tac"));
+        System.out.println("DOGG?GOD: " + func1("dogg", "god"));
+        System.out.println();
+        System.out.println("TAC?CAT: " + func2("tac", "cat"));
+        System.out.println("DOG?GOD: " + func2("dog", "god"));
+        System.out.println("TIC?TAC: " + func2("tic", "tac"));
+        System.out.println("DOGG?GOD: " + func2("dogg", "god"));
+
+    }
+
+    public static boolean func1(String input1, String input2)
+    {
+        if(input1.length() != input2.length())
+            return false;
+        else
+        {
+            if(getStrCharCount(input1).equals(getStrCharCount(input2)))
+                return true;
+            else
+                return false;
+        }
+    }
+
+    public static HashMap<Character, Integer> getStrCharCount(String input)
+    {
+        char[] chars = input.toCharArray();
+        HashMap<Character, Integer> counts = new HashMap<>();
+
+
+        for(Character x : chars)
+        {
+            if(counts.containsKey(x))
+                counts.put(x, counts.get(x) + 1);
+            else
+                counts.put(x, 0);
+        }
+
+        return counts;
+    }
+
+    public static boolean func2(String input1, String input2)
+    {
+        if(input1.length() != input2.length())
+            return false;
+        else
+        {
+            char[] input1Chars = input1.toCharArray();
+            char[] input2Chars = input2.toCharArray();
+
+            Arrays.sort(input1Chars);
+            Arrays.sort(input2Chars);
+
+            if(Arrays.equals(input1Chars, input2Chars))
+                return true;
+            else
+                return false;
+
+        }
+    }
+}
---
+++ src/io/tanners/UniqueStrings.java
@@ -0,0 +1,67 @@
+package io.tanners;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * Check if a string has unique chars using a data structure and without one
+ * 1.1
+ *
+ *
+ *
+ */
+
+public class UniqueStrings {
+
+    private static final int ALPHA_COUNT = 26;
+
+    public static void main(String[] args) {
+        String testCase1 = "This is a example string";
+        String testCase2 = "abcdefghijklmnopqrstuvwxyz";
+
+        System.out.println("WTIH DB STR 1: " + withDataStructure(testCase1));
+        System.out.println("WTIH DB STR 2: " + withDataStructure(testCase2));
+        System.out.println("WTIHOUT DB STR 1: " + withoutDataStructure(testCase1));
+        System.out.println("WTIHOUT DB STR 2: " + withoutDataStructure(testCase2));
+    }
+
+
+    public static boolean withDataStructure(String input)
+    {
+        char chars[] = input.toLowerCase().toCharArray();
+        HashSet<Character> structure = new HashSet<Character>();
+
+        for(Character x : chars)
+        {
+            if(structure.contains(x))
+                return false;
+            else
+                structure.add(x);
+        }
+
+        return true;
+    }
+
+    public static boolean withoutDataStructure(String input)
+    {
+        char chars[] = input.toLowerCase().toCharArray();
+        boolean charBool[] = new boolean[ALPHA_COUNT];
+
+        for(Character x : chars)
+        {
+            if(!Character.isWhitespace(x))
+            {
+                int offset = x - 97;
+
+                if(charBool[offset])
+                    return false;
+                else
+                    charBool[offset] = true;
+            }
+        }
+
+        return true;
+    }
+}
+
+
