90a55e4fefe9bcc6aa219cd4763a448e2506b559
tanners <tannersummers@gmail.com>
2017-07-11 22:28:51 -0700

    - QueueArray now at O(1) for all operations except when it is full and has to grow

--- ctc/src/main/java/StacksAndQueues/QueueArray.java
+++ ctc/src/main/java/StacksAndQueues/QueueArray.java
@@ -1,7 +1,98 @@
 package StacksAndQueues;
 
 
+import java.util.Arrays;
+
 public class QueueArray<E> extends Queue implements IQueue{
+
+    public static void main (String[] args)
+    {
+
+            QueueArray<Integer> testQueue = new QueueArray<Integer>();
+
+            testQueue.enqueue(1);
+            testQueue.enqueue(2);
+            testQueue.enqueue(3);
+            testQueue.enqueue(4);
+            testQueue.enqueue(5);
+            testQueue.enqueue(6);
+            testQueue.enqueue(7);
+            testQueue.enqueue(8);
+            testQueue.enqueue(9);
+
+        System.out.println(testQueue.toString());
+
+        testQueue.dequeue();
+        testQueue.dequeue();
+
+        System.out.println(testQueue.toString());
+
+
+
+            testQueue.enqueue(10);
+            testQueue.enqueue(11);
+//
+            System.out.println(testQueue.toString());
+
+            testQueue.enqueue(12);
+
+
+////
+            System.out.println(testQueue.toString());
+////
+            testQueue.enqueue(13);
+
+
+
+//
+            System.out.println(testQueue.toString());
+
+        testQueue.enqueue(14);
+        testQueue.enqueue(15);
+        testQueue.enqueue(16);
+        System.out.println(testQueue.toString());
+        testQueue.dequeue();
+        testQueue.dequeue();
+
+        System.out.println(testQueue.toString());
+        testQueue.dequeue();
+        testQueue.dequeue();
+        testQueue.dequeue();
+        testQueue.dequeue();
+        testQueue.dequeue();
+        testQueue.dequeue();
+        testQueue.dequeue();
+        testQueue.dequeue();
+        testQueue.dequeue();
+        testQueue.dequeue();
+        testQueue.dequeue();
+        testQueue.dequeue();
+
+
+
+        System.out.println(testQueue.toString());
+
+//        testQueue.dequeue();
+
+
+
+        System.out.println(testQueue.toString());
+
+//        assertArrayEquals(testQueue.getQueue(), new Integer[]{});
+
+//        assertEquals("PEEK == 11", testQueue.peek().intValue(), 11);
+
+
+    }
+
+    public E[] getQueue() {
+        return queue;
+    }
+
+    public void setQueue(E[] queue) {
+        this.queue = queue;
+    }
+
     // queue
     private E queue[];
     // default size
@@ -10,10 +101,10 @@ public class QueueArray<E> extends Queue implements IQueue{
     private int end;
     // basically a cursor to point to which element for the queue is at front
     private int start;
-
-
     // check to see if array is expandable
     private boolean limit;
+
+    private int size;
 
     /**
      *
@@ -24,12 +115,12 @@ public class QueueArray<E> extends Queue implements IQueue{
         // set queue size
         this.queue = (E[]) new Object[default_size];
         // set index to a non index array element
-        this.end = -1;
+        this.end = 0;
         // set limit to false
         this.limit = false;
 
-
-        this.start = -1;
+        this.size = 0;
+        this.start = 0;
     }
 
     /**
@@ -48,18 +139,17 @@ public class QueueArray<E> extends Queue implements IQueue{
                 // set queue size
                 this.queue = (E[]) new Object[size];
                 // set index to a non index array element
-                this.end = -1;
+                this.end = 0;
                 // set limit to false
                 this.limit = false;
 
-
-                this.start = -1;
+                this.size = 0;
+                this.start = 0;
             }
         } catch (NegativeNumberException e) {
             e.printStackTrace();
         }
     }
-
 
     /**
      * size of the queue
@@ -78,9 +168,13 @@ public class QueueArray<E> extends Queue implements IQueue{
                 // set queue size
                 this.queue = (E[]) new Object[size];
                 // set index to a non index array element
-                this.index = -1;
+                this.end = 0;
                 // set limit
                 this.limit = limit;
+
+                this.size = 0;
+                this.start = 0;
+
             }
         }
         catch (NegativeNumberException e) {
@@ -98,7 +192,7 @@ public class QueueArray<E> extends Queue implements IQueue{
             // make sure queue has any data by using index value
             if(!isEmpty())
                 // return element at that index
-                return (E) this.queue[index];
+                return (E) this.queue[start];
             // queue is empty
             else
                 throw new QueueEmptyException("Queue is empty");
@@ -117,34 +211,66 @@ public class QueueArray<E> extends Queue implements IQueue{
     @Override
     public void enqueue(Object data){
         try {
-            // index starts at -1, increment for first time
-            // or any other times due to previous insertion
-            this.index++;
-            // check if queue can fit the new data
-            if(this.index >= this.queue.length)
+//            System.out.println("DEBUG 1: " + this.size);
+//            System.out.println("DEBUG 1.1: " + this.queue.length);
+//            System.out.println("DEBUG 1.2: " + this.end);
+//            System.out.println("DEBUG 1.3: " + this.queue.length);
+
+
+            // if queue as at end and full
+//            if((this.end >= this.queue.length) && (this.size == this.queue.length))
+            if((this.size == this.queue.length))
             {
+//                System.out.println("DEBUG 2");
+
                 // check if queue is expandable
                 if(!this.limit)
                 {
+
+//                    System.out.println("DEBUG 3");
+
                     // increase size of array
                     expandQueue();
                     // add data to queue
-                    this.queue[this.index] = (E) data;
+                    this.queue[this.end] = (E) data;
                 }
                 // queue can't be expanded
                 else
                     throw new QueueFullException("Queue is full and cannot be expanded");
-
             }
-            // queue can fit more data
+            // queue is at end but not full
+            else if((this.end >= this.queue.length) && (this.size != this.queue.length))
+            {
+
+//                System.out.println("DEBUG 4");
+
+
+                //int newPos = this.end % this.queue.length;
+
+                // the new value will wrap around back to first element
+                // this is assumed empty space since the size var != size of queue
+                this.queue[0] = (E) data;
+                // reset the end to new value
+                this.end = 0;
+            }
             else
             {
-                // add data to queue
-                this.queue[index] = (E) data;
+//                System.out.println("DEBUG 5:  " + data);
+                this.queue[this.end] = (E) data;
             }
+
+            // increase size
+            this.size++;
+
+            // index starts at -1, increment for first time
+            // or any other times due to previous insertion
+            this.end++;
         } catch (QueueFullException e) {
             e.printStackTrace();
         }
+
+
+//        System.out.println("DEBUG &: " + Arrays.toString(this.queue));
     }
 
     /**
@@ -156,10 +282,61 @@ public class QueueArray<E> extends Queue implements IQueue{
     {
         // increase new queue at double the current size
         E newQueue[] = (E[]) new Object[this.queue.length * 2];
-        // loop and move elements from new queue to the new queue
-        System.arraycopy(this.queue, 0, newQueue, 0, this.queue.length);
-        // set new queue
+//        System.out.println("DEBUG 33");
+
+
+
+
+        int i = 0;
+
+        for(int j = this.end ; i < this.size; i++, j++)
+        {
+//            System.out.println("DEBUG 34");
+
+//            System.out.println("DEBUG !!: " + j);
+//            System.out.println("DEBUG ##: " + (this.queue.length-1));
+
+//            newQueue[i] = this.queue[j];
+
+            // if start is at last element
+            if(j == this.queue.length)
+            {
+
+                j = 0;
+
+
+                newQueue[i] = this.queue[j];
+
+
+
+            }
+            else
+            {
+
+                newQueue[i] = this.queue[j];
+            }
+
+
+
+
+        }
+
+        this.start = 0;
+        this.end = i;
+//        this.size
+
+//        System.out.println("DEBUG 34");
+
+//        System.out.println("DEBUG &%%: " + Arrays.toString(newQueue));
+
+
+//        // loop and move elements from new queue to the new queue
+//        System.arraycopy(this.queue, 0, newQueue, 0, this.queue.length);
+//        // set new queue
         this.queue = newQueue;
+
+
+
     }
 
     /**
@@ -169,16 +346,27 @@ public class QueueArray<E> extends Queue implements IQueue{
     @Override
     public E dequeue() {
         try {
-
-
             // check if queue is not empty
             if (!isEmpty()) {
+                // decrement size
+                this.size--;
                 // get element
-                E element = this.queue[0];
+                E element = this.queue[this.start];
                 // reset element value
-                this.queue[0] = null;
+                this.queue[this.start] = null;
                 // reduce index pointer
-                this.index--;
+                this.start++;
+//                // if empty after last dequeue
+                if(isEmpty())
+                {
+                    // reset values
+                    this.end = 0;
+                    this.start = 0;
+                }
+
+//                System.out.println("REMOVED VALUE: " + element);
+//                System.out.println("NEXT VALUE: " + this.queue[this.start]);
+
                 // return value
                 return element;
             } else
@@ -203,9 +391,43 @@ public class QueueArray<E> extends Queue implements IQueue{
         // check where index is pointing to in queue
         // this will tell if it is empty or not
         // if index is a 0 + or less than array size (this may always be true but worth checking)
-        if(this.index >= 0 && this.index < this.queue.length)
-            return false;
-        else
-            return true;
+//        if(this.index >= 0 && this.index < this.queue.length)
+//        return this.size < this.queue.length;
+        return this.size == 0;
+    }
+
+    @Override
+    public String toString()
+    {
+        StringBuffer str = new StringBuffer();
+
+//        System.out.println("SIZE: " + size);
+
+//        for(int i = 0; i < this.size; i++)
+        for(int i = 0; i < this.queue.length; i++)
+        {
+//            if(this.end % this.queue.length == 0)
+            // if cursor is at end of the queue
+            // it needs to wrap around
+//            if(j == this.queue.length)
+//            {
+//                j = 0;
+//                str.append(this.queue[j]);
+//                str.append(", ");
+//            }
+//            else
+//            {
+//                str.append(this.queue[j]);
+//                str.append(", ");
+//            }
+
+            str.append(this.queue[i]);
+            str.append(", ");
+
+        }
+
+//        System.out.println(str.toString());
+
+        return str.toString();
     }
 }
--- ctc/src/test/java/TestQueueArray.java
+++ ctc/src/test/java/TestQueueArray.java
@@ -4,93 +4,362 @@ import StacksAndQueues.QueueArray;
 import StacksAndQueues.QueueLinkedList;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+
+import static org.junit.Assert.*;
 
 public class TestQueueArray {
 
-    @Test
-    public void TestEmptyAdd()
-    {
-        QueueArray<Integer> testQueue = new QueueArray<Integer>();
-       testQueue.enqueue(1);
-       assertEquals("PEEK == 1", testQueue.peek().intValue(), 1);
-    }
+//    @Test
+//    public void TestEmptyAdd()
+//    {
+//        QueueArray<Integer> testQueue = new QueueArray<Integer>();
+//       testQueue.enqueue(1);
+//       assertEquals("PEEK == 1", testQueue.peek().intValue(), 1);
+//    }
+//
+//    @Test
+//    public void TestEmptyAdd2()
+//    {
+//        QueueArray<Integer> testQueue = new QueueArray<Integer>();
+//        testQueue.enqueue(1);
+//        testQueue.enqueue(4);
+//        assertEquals("PEEK == 1", testQueue.peek().intValue(), 1);
+//    }
+//
+//    @Test
+//    public void TestEmptyAdd3()
+//    {
+//        QueueArray<Integer> testQueue = new QueueArray<Integer>();
+//        testQueue.enqueue(1);
+//        testQueue.enqueue(2);
+//        testQueue.enqueue(3);
+//        testQueue.enqueue(4);
+//        testQueue.enqueue(5);
+//        assertEquals("PEEK == 1", testQueue.peek().intValue(), 1);
+//    }
+//
+//    @Test
+//    public void TestEmptyAdd4()
+//    {
+//        QueueArray<Integer> testQueue = new QueueArray<Integer>();
+//        testQueue.enqueue(1);
+//        testQueue.enqueue(2);
+//        testQueue.enqueue(3);
+//        testQueue.enqueue(4);
+//        testQueue.enqueue(5);
+//        testQueue.enqueue(6);
+//        testQueue.enqueue(7);
+//        testQueue.enqueue(8);
+//        testQueue.enqueue(9);
+//        testQueue.enqueue(10);
+//
+//        System.out.println(testQueue.toString());
+//
+//        assertEquals("PEEK == 1", testQueue.peek().intValue(), 1);
+//    }
+
+//    @Test
+//    public void TestEmptyAdd5()
+//    {
+//        QueueArray<Integer> testQueue = new QueueArray<Integer>();
+//        testQueue.enqueue(1);
+//        testQueue.enqueue(2);
+//        testQueue.enqueue(3);
+//        testQueue.enqueue(4);
+//        testQueue.enqueue(5);
+//        testQueue.enqueue(6);
+//        testQueue.enqueue(7);
+//        testQueue.enqueue(8);
+//        testQueue.enqueue(9);
+//        testQueue.enqueue(10);
+//        testQueue.enqueue(11);
+//        testQueue.enqueue(12);
+//        testQueue.enqueue(13);
+//
+//        System.out.println(testQueue.toString());
+//
+//        assertEquals("PEEK == 1", testQueue.peek().intValue(), 1);
+//    }
+
+//    @Test
+//    public void TestEmptyAddDel()
+//    {
+//        QueueArray<Integer> testQueue = new QueueArray<Integer>();
+//        testQueue.enqueue(4);
+//        testQueue.enqueue(2);
+//        testQueue.enqueue(3);
+//
+////        System.out.println(testQueue.toString());
+//
+//        int shouldBe4 = testQueue.dequeue();
+//        int shouldBe2 = testQueue.dequeue();
+//
+////        System.out.println("4=?: " + shouldBe4);
+////        System.out.println("2=?: " + shouldBe2);
+//
+//        assertEquals("SHOULD BE 4 REMOVED" , shouldBe4, 4);
+//        assertEquals("SHOULD BE 2 REMOVED" , shouldBe2, 2);
+//
+//        System.out.println(testQueue.toString());
+//
+//        assertEquals("PEEK == 3", testQueue.peek().intValue(), 3);
+//    }
+
+//    @Test
+//    public void TestEmptyAddDel2()
+//    {
+//        QueueArray<Integer> testQueue = new QueueArray<Integer>();
+//        testQueue.enqueue(4);
+//        testQueue.enqueue(2);
+//        testQueue.enqueue(3);
+//
+////        System.out.println(testQueue.toString());
+//
+//        int shouldBe4 = testQueue.dequeue();
+//        int shouldBe2 = testQueue.dequeue();
+//        int shouldBe3 = testQueue.dequeue();
+////        System.out.println("4=?: " + shouldBe4);
+////        System.out.println("2=?: " + shouldBe2);
+//
+//        assertEquals("SHOULD BE 4 REMOVED" , shouldBe4, 4);
+//        assertEquals("SHOULD BE 2 REMOVED" , shouldBe2, 2);
+//        assertEquals("SHOULD BE 3 REMOVED" , shouldBe3, 3);
+//        assertTrue("Should be empty : true", testQueue.isEmpty());
+//
+//        System.out.println(testQueue.toString());
+//
+//
+////        assertEquals("PEEK == 3", testQueue.peek().intValue(), 3);
+//    }
+
+
+//    @Test
+//    public void TestEmptyAddDelAdd()
+//    {
+//        QueueArray<Integer> testQueue = new QueueArray<Integer>();
+//        testQueue.enqueue(4);
+//        testQueue.enqueue(2);
+//        testQueue.enqueue(3);
+//
+////        System.out.println(testQueue.toString());
+//
+//        int shouldBe4 = testQueue.dequeue();
+//        int shouldBe2 = testQueue.dequeue();
+//        int shouldBe3 = testQueue.dequeue();
+////        System.out.println("4=?: " + shouldBe4);
+////        System.out.println("2=?: " + shouldBe2);
+//
+//        assertEquals("SHOULD BE 4 REMOVED" , shouldBe4, 4);
+//        assertEquals("SHOULD BE 2 REMOVED" , shouldBe2, 2);
+//        assertEquals("SHOULD BE 3 REMOVED" , shouldBe3, 3);
+//        assertTrue("Should be empty : true", testQueue.isEmpty());
+//
+//        System.out.println(testQueue.toString());
+//
+//
+//        testQueue.enqueue(5);
+//        testQueue.enqueue(6);
+//        testQueue.enqueue(7);
+//        System.out.println(testQueue.toString());
+//
+//        assertEquals("PEEK == 5", testQueue.peek().intValue(), 5);
+//    }
+
+
+//    @Test
+//    public void TestEmptyAddDelAdd2()
+//    {
+//        QueueArray<Integer> testQueue = new QueueArray<Integer>();
+//        testQueue.enqueue(4);
+//        testQueue.enqueue(2);
+//        testQueue.enqueue(3);
+//
+////        System.out.println(testQueue.toString());
+//
+//        int shouldBe4 = testQueue.dequeue();
+//        int shouldBe2 = testQueue.dequeue();
+//        int shouldBe3 = testQueue.dequeue();
+////        System.out.println("4=?: " + shouldBe4);
+////        System.out.println("2=?: " + shouldBe2);
+//
+//        assertEquals("SHOULD BE 4 REMOVED" , shouldBe4, 4);
+//        assertEquals("SHOULD BE 2 REMOVED" , shouldBe2, 2);
+//        assertEquals("SHOULD BE 3 REMOVED" , shouldBe3, 3);
+//        assertTrue("Should be empty : true", testQueue.isEmpty());
+//
+//        System.out.println(testQueue.toString());
+//
+//
+//        testQueue.enqueue(1);
+//        testQueue.enqueue(2);
+//        testQueue.enqueue(3);
+//        testQueue.enqueue(4);
+//        testQueue.enqueue(5);
+//        testQueue.enqueue(6);
+//        testQueue.enqueue(7);
+//        testQueue.enqueue(8);
+//        testQueue.enqueue(9);
+//
+//        System.out.println(testQueue.toString());
+//
+//        assertEquals("SHOULD BE 1 REMOVED" , testQueue.dequeue().intValue(), 1);
+//        assertEquals("SHOULD BE 2 REMOVED" , testQueue.dequeue().intValue(), 2);
+//
+//        System.out.println(testQueue.toString());
+//
+//
+//
+//        testQueue.enqueue(10);
+//        testQueue.enqueue(11);
+//
+//        System.out.println(testQueue.toString());
+//
+//        testQueue.enqueue(12);
+//
+//        System.out.println(testQueue.toString());
+//
+////        testQueue.enqueue(13);
+//
+////        System.out.println(testQueue.toString());
+//    }
+
+//    @Test
+//    public void TestEmptyPreExpand()
+//    {
+//        QueueArray<Integer> testQueue = new QueueArray<Integer>();
+//
+//        testQueue.enqueue(1);
+//        testQueue.enqueue(2);
+//        testQueue.enqueue(3);
+//        testQueue.enqueue(4);
+//        testQueue.enqueue(5);
+//        testQueue.enqueue(6);
+//        testQueue.enqueue(7);
+//        testQueue.enqueue(8);
+//        testQueue.enqueue(9);
+//
+//        System.out.println(testQueue.toString());
+//
+//        assertEquals("SHOULD BE 1 REMOVED" , testQueue.dequeue().intValue(), 1);
+//        assertEquals("SHOULD BE 2 REMOVED" , testQueue.dequeue().intValue(), 2);
+//
+//        System.out.println(testQueue.toString());
+//
+//
+//
+//        testQueue.enqueue(10);
+//        testQueue.enqueue(11);
+//
+//        System.out.println(testQueue.toString());
+//
+//        testQueue.enqueue(12);
+//
+//        System.out.println(testQueue.toString());
+//
+//    }
 
     @Test
-    public void TestEmptyAdd2()
+    public void TestEmptyPostExpand()
     {
         QueueArray<Integer> testQueue = new QueueArray<Integer>();
+
         testQueue.enqueue(1);
         testQueue.enqueue(2);
         testQueue.enqueue(3);
         testQueue.enqueue(4);
         testQueue.enqueue(5);
-        assertEquals("PEEK == 5", testQueue.peek().intValue(), 5);
-    }
-
-    @Test
-    public void TestEmptyAddDel()
-    {
-        QueueArray<Integer> testQueue = new QueueArray<Integer>();
-        testQueue.enqueue(4);
-        testQueue.enqueue(2);
-        testQueue.enqueue(3);
-        testQueue.dequeue();
-        testQueue.dequeue();
-        assertEquals("PEEK == 4", testQueue.peek().intValue(), 4);
-    }
-
-    @Test
-    public void TestEmptyAddDelAdd()
-    {
-        QueueArray<Integer> testQueue = new QueueArray<Integer>();
-        testQueue.enqueue(4);
-        testQueue.enqueue(2);
-        testQueue.enqueue(3);
-        testQueue.dequeue();
-        testQueue.dequeue();
-        testQueue.enqueue(8);
-        testQueue.enqueue(9);
-        assertEquals("PEEK == 9", testQueue.peek().intValue(), 9);
-    }
-
-    @Test
-    public void TestEmptyAddDelAll()
-    {
-        QueueArray<Integer> testQueue = new QueueArray<Integer>();
-        testQueue.enqueue(4);
-        testQueue.enqueue(2);
-        testQueue.enqueue(3);
-        testQueue.dequeue();
-        testQueue.dequeue();
-        testQueue.dequeue();
-        testQueue.enqueue(5);
         testQueue.enqueue(6);
         testQueue.enqueue(7);
-        testQueue.dequeue();
-        testQueue.dequeue();
-        testQueue.dequeue();
+        testQueue.enqueue(8);
+        testQueue.enqueue(9);
 
-        assertTrue("SHOULD BE EMPTY", testQueue.isEmpty());
+//        System.out.println(testQueue.toString());
+
+//        assertEquals("SHOULD BE 1 REMOVED" , testQueue.dequeue().intValue(), 1);
+//        assertEquals("SHOULD BE 2 REMOVED" , testQueue.dequeue().intValue(), 2);
+
+//        System.out.println(testQueue.toString());
+
+
+
+        testQueue.enqueue(10);
+//        testQueue.enqueue(11);
+//
+        System.out.println(testQueue.toString());
+
+//        testQueue.enqueue(12);
+
+        testQueue.enqueue(11);
+////
+//        System.out.println(testQueue.toString());
+////
+////        testQueue.enqueue(13);
+
+
+
+//
+//        System.out.println(testQueue.toString());
+//        System.out.println(testQueue.toString());
+//        System.out.println(testQueue.toString());
+//        System.out.println(testQueue.toString());
+
+//        assertArrayEquals(testQueue.getQueue(), new Integer[]{});
+
+//        assertEquals("PEEK == 11", testQueue.peek().intValue(), 11);
+
     }
 
-    @Test
-    public void TestEmptyAddDelAll2()
-    {
-        QueueArray<Integer> testQueue = new QueueArray<Integer>();
 
-        assertTrue("SHOULD BE EMPTY2", testQueue.isEmpty());
-    }
-
-    @Test
-    public void TestEmptyAddDelAll3()
-    {
-        QueueArray<Integer> testQueue = new QueueArray<Integer>();
-        testQueue.enqueue(4);
-        testQueue.dequeue();
-        assertTrue("SHOULD BE EMPTY3", testQueue.isEmpty());
-    }
+//
+//    @Test
+//    public void TestEmptyAddDelAdd()
+//    {
+//        QueueArray<Integer> testQueue = new QueueArray<Integer>();
+//        testQueue.enqueue(4);
+//        testQueue.enqueue(2);
+//        testQueue.enqueue(3);
+//        testQueue.dequeue();
+//        testQueue.dequeue();
+//        testQueue.enqueue(8);
+//        testQueue.enqueue(9);
+//        assertEquals("PEEK == 9", testQueue.peek().intValue(), 9);
+//    }
+//
+//    @Test
+//    public void TestEmptyAddDelAll()
+//    {
+//        QueueArray<Integer> testQueue = new QueueArray<Integer>();
+//        testQueue.enqueue(4);
+//        testQueue.enqueue(2);
+//        testQueue.enqueue(3);
+//        testQueue.dequeue();
+//        testQueue.dequeue();
+//        testQueue.dequeue();
+//        testQueue.enqueue(5);
+//        testQueue.enqueue(6);
+//        testQueue.enqueue(7);
+//        testQueue.dequeue();
+//        testQueue.dequeue();
+//        testQueue.dequeue();
+//
+//        assertTrue("SHOULD BE EMPTY", testQueue.isEmpty());
+//    }
+//
+//    @Test
+//    public void TestEmptyAddDelAll2()
+//    {
+//        QueueArray<Integer> testQueue = new QueueArray<Integer>();
+//
+//        assertTrue("SHOULD BE EMPTY2", testQueue.isEmpty());
+//    }
+//
+//    @Test
+//    public void TestEmptyAddDelAll3()
+//    {
+//        QueueArray<Integer> testQueue = new QueueArray<Integer>();
+//        testQueue.enqueue(4);
+//        testQueue.dequeue();
+//        assertTrue("SHOULD BE EMPTY3", testQueue.isEmpty());
+//    }
 
 }
